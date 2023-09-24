fadc55704091a7b2e212ac37aaf22173337a8930
Ansonsd5 <anson11sd@gmail.com>
2023-09-24 23:17:44 +0530

    signout btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
 
 const Header = () => {
-
+const navigate = useNavigate();
   const avatarClick = () => {
-    alert("Avatar Clicked")
+    
+  }
+
+  const handleSignOut = () =>{
+    signOut(auth).then(() => {
+      navigate("/");
+    }).catch((error) => {
+      navigate("/error")
+    });
+    
   }
   return (
     <div className=" px-8 py-2 bg-gradient-to-b from-black flex justify-between items-center">
@@ -11,8 +23,11 @@ const Header = () => {
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="netflix-icon"
       />
+      <div className="flex items-center gap-4">
       <button onClick={() =>  avatarClick() }>
-      <img className="w-8 h-8 rounded-sm bg-clip-border" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"/></button>
+      <img className="w-8 h-8 rounded-xl bg-clip-border" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="icon"/></button>
+      <button onClick={()=> handleSignOut()} className="font-bold bg-red-600 px-4 py-1 rounded-sm">Sign Out</button>
+      </div>
     </div>
   );
 };
